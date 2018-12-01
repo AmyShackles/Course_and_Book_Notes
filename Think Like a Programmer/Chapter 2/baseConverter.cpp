4ffdd4ab2a74c6067f4072de1b257fa2a03d53dc
AmyShackles <amyshackles@gmail.com>
2018-12-01 01:19:41 -0800

    Refactored baseConverter

--- Think Like a Programmer/Chapter 2/baseConverter.cpp
+++ Think Like a Programmer/Chapter 2/baseConverter.cpp
@@ -3,36 +3,60 @@ using std::cin;
 using std::cout;
 
 // If entering a binary number, preface with 0b (e.g.: 0b100111111011001)
-// If entering a hexadecimal number, preface with 0x (e.g.: 0xDEADBEE)
+// If entering a hexadecimal number, preface with 0x (e.g.: 0xDEADBEEF)
 // If entering an octal number, preface with 0 (e.g.: 077)
 
-int inferBase(char digitChar) {
-  if (digitChar == 'b') {
-    return 2;
-  } else if (digitChar == 'x') {
-    return 16;
+int inferBase(char *string) {
+  if (string[0] == '0') {
+    if (string[1] == 'b') {
+      return 2;
+    } else if (string[1] == 'x') {
+      return 16;
+    } else {
+      return 8;
+    }
   } else {
-    return 8;
+    return 10;
   }
 }
 
-long long digitValue(char digitChar) {
-  if ((digitChar - '0') > 9) {
-    digitChar = (digitChar - '0') - 7;
-  } else {
-    digitChar = digitChar - '0';
-  }
-  return digitChar;
-}
-
-long long convertToDecimal(long long number, char digitChar, int base) {
-  if (number > 9) {
-    number = number - 7;
-  }
-  while (digitChar != 10) {
-
-    number = number * base + digitValue(digitChar);
+char *parseString(char digitChar) {
+  char *string = new char[100];
+  int i = 0;
+  while (digitChar != '\n') {
+    string[i] = digitChar;
     digitChar = cin.get();
+    i++;
+  }
+  string[i] = '\0';
+  cout << "String: " << string << "\n";
+  return string;
+}
+
+long long convertToInt() {
+  char digitChar = cin.get();
+  char *string = parseString(digitChar);
+  int base = inferBase(string);
+  long long number = 0;
+  int i = 0;
+  if (base == 16 || base == 2) {
+    // Need to get next digit for base 16 and base 2
+    // ... because current digit is still just information about base
+    i = 2;
+  } else if (base == 8) {
+    i = 1;
+  }
+  while (string[i] != '\0') {
+    string[i] = string[i] - '0';
+    // The capitalized alphabet is offset from '0' - '9' by 7 numbers.
+    // If we subtract '0' from a letter, we're 7 digits off from where we should
+    // be For example, 'B' in ASCII is 66, '0' is 48 66 - 48 = 18, but 'B'
+    // represents 11.
+    if (string[i] > 9) {
+      string[i] = string[i] - 7;
+    }
+    number = number * base + string[i];
+    i++;
   }
   return number;
 }
@@ -60,6 +84,9 @@ char *intToBaseString(long long number, int base) {
     remainder = number % base;
     number /= base;
     if (remainder > 9) {
+      // if remainder is greater than int 9, subtract 10
+      // this takes you back to 0 in the ASCII table,
+      // allowing you to add 'A' to get in range of the alphabet
       output = remainder - 10 + 'A';
       str[i] = output;
     } else {
@@ -78,8 +105,7 @@ int main() {
           "number, preface with '0b'.\nIf entering a hexadecimal number, "
           "preface with '0x'.\nIf entering an octal number, preface with "
           "'0'\n>> ";
-  char digitChar = cin.get();
-  long long dec;
+  long long integer;
   char *binary;
   char *ternary;
   char *quaternary;
@@ -94,37 +120,18 @@ int main() {
   int number = 0;
   int base;
 
-  // Need to keep this first test of whether or not the digitChar is '0' here
-  // because if it's moved to the inferBase function, we lose access to the
-  // first real index of an octal value (as it will only exist in inferBase)
-  if ((digitChar - '0') == 0) {
-    digitChar = cin.get();
-    base = inferBase(digitChar);
-  } else {
-    base = 10;
-  }
-  if (base == 16 || base == 2) {
-    cout << "Base: " << base << "\n";
-    digitChar = cin.get();
-    number = (digitChar - '0');
-    digitChar = cin.get();
-  } else if (base == 10 || base == 8) {
-    cout << "Base: " << base << "\n";
-    number = (digitChar - '0');
-    digitChar = cin.get();
-  }
-  dec = convertToDecimal(number, digitChar, base);
-  binary = intToBaseString(dec, 2);
-  ternary = intToBaseString(dec, 3);
-  quaternary = intToBaseString(dec, 4);
-  quinary = intToBaseString(dec, 5);
-  senary = intToBaseString(dec, 6);
-  octal = intToBaseString(dec, 8);
-  decimal = intToBaseString(dec, 10);
-  duodecimal = intToBaseString(dec, 12);
-  hexadecimal = intToBaseString(dec, 16);
-  vigesimal = intToBaseString(dec, 20);
-  base36 = intToBaseString(dec, 36);
+  integer = convertToInt();
+  binary = intToBaseString(integer, 2);
+  ternary = intToBaseString(integer, 3);
+  quaternary = intToBaseString(integer, 4);
+  quinary = intToBaseString(integer, 5);
+  senary = intToBaseString(integer, 6);
+  octal = intToBaseString(integer, 8);
+  decimal = intToBaseString(integer, 10);
+  duodecimal = intToBaseString(integer, 12);
+  hexadecimal = intToBaseString(integer, 16);
+  vigesimal = intToBaseString(integer, 20);
+  base36 = intToBaseString(integer, 36);
 
   cout << "Number entered (in decimal): " << decimal << "\n";
   cout << "Number entered (in binary): " << binary << "\n";
