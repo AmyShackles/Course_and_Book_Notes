26462838bd6dfa1e6ee90ee059c6337e01aa3a00
AmyShackles <amyshackles@gmail.com>
2018-12-10 11:54:59 -0800

    Improved on the program to sort student grades, including a method to sort by lowest and by highest

--- Think Like a Programmer/Chapter 3/3-1.c
+++ Think Like a Programmer/Chapter 3/3-1.c
@@ -7,13 +7,38 @@ typedef struct student {
   char *name;
 } student;
 
-int compareFunc(const void *voidA, const void *voidB) {
+int lowToHigh(const void *voidA, const void *voidB) {
   int a = (*(student *)voidA).grade;
   int b = (*(student *)voidB).grade;
   if (a < b) {
     return -1;
   } else if (a > b) {
     return +1;
+  } else {
+    return 0;
+  }
+}
+
+int highToLow(const void *voidA, const void *voidB) {
+  int a = (*(student *)voidA).grade;
+  int b = (*(student *)voidB).grade;
+
+  if (a < b) {
+    return 1;
+  } else if (a > b) {
+    return -1;
+  } else {
+    return 0;
+  }
+}
+
+int compareIDs(const void *voidA, const void *voidB) {
+  int a = (*(student *)voidA).studentID;
+  int b = (*(student *)voidB).studentID;
+  if (a > b) {
+    return -1;
+  } else if (a < b) {
+    return 1;
   } else {
     return 0;
   }
@@ -30,10 +55,28 @@ int main() {
   int w = sizeof(struct student);
 
   int size = sizeof(studentArray) / sizeof(studentArray[0]);
-  qsort(studentArray, 10, w, compareFunc);
+  qsort(studentArray, 10, w, lowToHigh);
+  printf("Student Grades, Low to High: \n");
   for (int i = 0; i < size; i++) {
-    printf("%d ", studentArray[i].grade);
+    printf("{Student %d, Grade: %d}\n", studentArray[i].studentID,
+           studentArray[i].grade);
   }
   printf("\n");
+
+  qsort(studentArray, 10, w, highToLow);
+  printf("Student Grades, High to Low: \n");
+  for (int i = 0; i < size; i++) {
+    printf("{Student %d, Grade: %d}\n", studentArray[i].studentID,
+           studentArray[i].grade);
+  }
+  printf("\n");
+
+  qsort(studentArray, 10, w, compareIDs);
+  for (int i = 0; i < size; i++) {
+    printf("{Student: %d, Grade: %d}\n", studentArray[i].studentID,
+           studentArray[i].grade);
+  }
+  printf("\n");
+
   return 0;
 }
